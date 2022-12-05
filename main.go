70121d4a9c80f255bc41b226028c9323bad1381c
zil0t <Alexanderaolik@gmail.com>
2022-12-05 23:24:25 +0300

    Update main.go

--- main.go
+++ main.go
@@ -5,28 +5,29 @@ import (
 	"net/http"
 )
 
-// Создается функция-обработчик "home", которая записывает байтовый слайс, содержащий
-// текст "Привет из Snippetbox" как тело ответа.
+// Обработчик главной страницы.
 func home(w http.ResponseWriter, r *http.Request) {
-
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
 	w.Write([]byte("Привет из Snippetbox"))
 }
 
+// Обработчик для отображения содержимого заметки.
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Отображение заметки..."))
+}
+
+// Обработчик для создания новой заметки.
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Форма для создания новой заметки..."))
+}
+
 func main() {
-	// Используется функция http.NewServeMux() для инициализации нового рутера, затем
-	// функцию "home" регистрируется как обработчик для URL-шаблона "/".
+	// Регистрируем два новых обработчика и соответствующие URL-шаблоны в
+	// маршрутизаторе servemux
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
-	// Используется функция http.ListenAndServe() для запуска нового веб-сервера.
-	// Мы передаем два параметра: TCP-адрес сети для прослушивания (в данном случае это "localhost:4000")
-	// и созданный рутер. Если вызов http.ListenAndServe() возвращает ошибку
-	// мы используем функцию log.Fatal() для логирования ошибок. Обратите внимание
-	// что любая ошибка, возвращаемая от http.ListenAndServe(), всегда non-nil.
 	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
