48f2f8a5a23a17015271630ac301cf356faf0b44
zil0t <Alexanderaolik@gmail.com>
2022-12-07 14:40:27 +0300

    8

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+// Создаем структуру `application` для хранения зависимостей всего веб-приложения.
+// Пока, что мы добавим поля только для двух логгеров, но
+// мы будем расширять данную структуру по мере усложнения приложения.
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
+	addr := flag.String("addr", ":4000", "Сетевой адрес веб-сервера")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	log.Println("Запуск сервера на http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Инициализируем новую структуру http.Server. Мы устанавливаем поля Addr и Handler, так
+	// что сервер использует тот же сетевой адрес и маршруты, что и раньше, и назначаем
+	// поле ErrorLog, чтобы сервер использовал наш логгер
+	// при возникновении проблем.
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Запуск сервера на %s", *addr)
+	// Вызываем метод ListenAndServe() от нашей новой структуры http.Server
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
